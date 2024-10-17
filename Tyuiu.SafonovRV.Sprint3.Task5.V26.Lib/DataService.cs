8f295027627087c60be96c9ec865d54540bb3c27
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-10-17 18:43:16 +0500

    Task5

--- Tyuiu.SafonovRV.Sprint3.Task5.V26.Lib/DataService.cs
+++ Tyuiu.SafonovRV.Sprint3.Task5.V26.Lib/DataService.cs
@@ -7,12 +7,12 @@ namespace Tyuiu.SafonovRV.Sprint3.Task5.V26.Lib
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             double sumSeries = 0;
-            int i, j;
-            for (i = stopValue1; i <= stopValue1; i++)
+            int i, k;
+            for (i = startValue1; i <= stopValue1; k++)
             {
-                for (j = stopValue2; j <= stopValue2; j++)
+                for (k = startValue2; k <= stopValue2; k++)
                 {
-                    sumSeries = sumSeries + (Math.Pow(x, j) + Math.Sin(j));
+                    sumSeries += 1 / (Math.Sin(k) + x);
                 }
             }
             return Math.Round(sumSeries, 3);
@@ -21,5 +21,9 @@ namespace Tyuiu.SafonovRV.Sprint3.Task5.V26.Lib
 }
 
 
-    
+
+
+
+
+
 
