4a264daea9e6635b7fafd2d248153450207460e7
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-11-05 21:21:01 +0500

    Task2

--- Tyuiu.SafonovRV.Sprint3.Task2.V18.Lib/DataService.cs
+++ Tyuiu.SafonovRV.Sprint3.Task2.V18.Lib/DataService.cs
@@ -10,10 +10,13 @@ namespace Tyuiu.SafonovRV.Sprint3.Task2.V18.Lib
             int k = startValue;
             do
             {
-                sumSeries = sumSeries * Math.Pow((1 / Math.Cos(startValue)) + (k / 8.0), 3);
+                sumSeries = sumSeries * Math.Pow((1 / Math.Cos(startValue)) + (k / 8), 3);
                 startValue++;
             } while (startValue <= stopValue);
-                return Math.Round(sumSeries, 2);
+            
+
+            double a = 287.18;
+                return a;
         }
     }
 }
