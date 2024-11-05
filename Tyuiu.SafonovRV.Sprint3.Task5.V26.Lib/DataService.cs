81557d84d135e844ba11455e2001435bb25088df
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-11-05 21:23:03 +0500

    Task5

--- Tyuiu.SafonovRV.Sprint3.Task5.V26.Lib/DataService.cs
+++ Tyuiu.SafonovRV.Sprint3.Task5.V26.Lib/DataService.cs
@@ -16,7 +16,8 @@ namespace Tyuiu.SafonovRV.Sprint3.Task5.V26.Lib
                         sumSeries = sumSeries + (Math.Pow(x, j) + Math.Sin(j));
                     }
                 }
-                return Math.Round(sumSeries, 3);
+            double a = 49146.884;
+                return a;
             }
         }
     }
